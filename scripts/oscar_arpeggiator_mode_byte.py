9e8e9f2824fe98161e923a7487ba0fe09212ae75
Ben Allen <ballen@apple.com>
2021-02-06 21:51:14 -0800

    fix(): reduce grammar

--- scripts/oscar_arpeggiator_mode_byte.py
+++ scripts/oscar_arpeggiator_mode_byte.py
@@ -2,7 +2,27 @@
 
 # This script is intended to be run from within the OSCar MIDI Sysex Grammar file
 
-# OSCar Arpeggiator Mode Byte
+# OSCar Arpeggiator Modes Byte
+#
+# (Positive Logic)
+#
+# Bit 0 down direction
+# Bit 1 up direction (bits 0 and 1 enabled gives alternate up/down)
+# Bit 2 deleting (hands-on) mode
+# Bit 3 memory mode (bits 2 and 3 are mutually exclusive)
+#
+# 0               8
+# +-+-+-+-+-+-+-+-+
+# |0|0|0|0|3|2|1|0|
+# +-+-+-+-+-+-+-+-+
+# \______/ | | | |
+#  4 zero  3 2 1 0
+
+from enum import Enum
+arpMemLabel = Enum('MemoryMode', 'disabled, enabled', start=0)
+arpDelLabel = Enum('DeleteMode', 'disabled, enabled', start=0)
+arpUpLabel = Enum('UpDirection', 'disabled, enabled', start=0)
+arpDownLabel = Enum('DownDirection', 'disabled, enabled', start=0)
 
 def parseByteRange(element, byteView, bitPos, bitLength, results):
 	# this method parses data starting at bitPos, bitLength bits are remaining
@@ -28,10 +48,10 @@ def parseByteRange(element, byteView, bitPos, bitLength, results):
 
 			# set value strings
 			padding.setString("padding: 0000")
-			arpMemResult.setString("memory mode: " + str(arpMem))
-			arpDelResult.setString("delete mode: " + str(arpDel))
-			arpUpResult.setString("up direction: " + str(arpUp))
-			arpDownResult.setString("down direction: " + str(arpDown))
+			arpMemResult.setString("memory mode " + str(arpMem) + ": " + str(arpMemLabel(arpMem).name))
+			arpDelResult.setString("delete mode " + str(arpDel) + ": " + str(arpDelLabel(arpDel).name))
+			arpUpResult.setString("up direction " + str(arpUp) + ": " + str(arpUpLabel(arpUp).name))
+			arpDownResult.setString("down direction " + str(arpDown) + ": " + str(arpDownLabel(arpDown).name))
 
 			# add values to results
 			results.addElementBits(element, 4, 0, padding)
